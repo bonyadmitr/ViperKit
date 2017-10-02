4e998aacb7f0cc36da145e6b92332604dee6abae
Bondar Yaroslav <bonyadmitr@gmail.com>
2017-10-02 20:57:47 +0300

    updated project for mac os

--- ViperKit/ViperKit.h
+++ ViperKit/ViperKit.h
@@ -6,14 +6,7 @@
 //  Copyright © 2017 Bondar Yaroslav. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import Foundation;
 
-//! Project version number for ViperKit.
 FOUNDATION_EXPORT double ViperKitVersionNumber;
-
-//! Project version string for ViperKit.
 FOUNDATION_EXPORT const unsigned char ViperKitVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ViperKit/PublicHeader.h>
-
-
